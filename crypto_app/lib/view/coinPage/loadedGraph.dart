b90e0019521a2300c707818e84f0241b0889158c
Jan Kubes <jan.kubes07@gmail.com>
2024-05-07 22:09:58 +0200

    Design of graph.

--- crypto_app/lib/view/coinPage/loadedGraph.dart
+++ crypto_app/lib/view/coinPage/loadedGraph.dart
@@ -10,50 +10,58 @@ class GraphOfThePrice extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 90),
-      child: LineChart(LineChartData(
-        gridData: const FlGridData(show: false),
-        borderData: FlBorderData(
-          show: false,
+      child: LineChart(
+        LineChartData(
+          titlesData: const FlTitlesData(show: false),
+          gridData: const FlGridData(show: false),
+          borderData: FlBorderData(
+            show: false,
+          ),
+          minX: 0,
+          minY: 0,
+          // This was quite sketchy to find how to do, but this returns what the top box looks like when you hover over graph.
+          lineTouchData: LineTouchData(
+            touchTooltipData: LineTouchTooltipData(
+              // Here I'm trying not to overlap
+              fitInsideHorizontally: true,
+              tooltipBgColor: Theme.of(context).primaryColor,
+              tooltipPadding: const EdgeInsets.all(5),
+              getTooltipItems: (List<LineBarSpot> touchedSpots) {
+                return touchedSpots.map(
+                  (barSpot) {
+                    DateTime date = DateTime.parse(prices[barSpot.spotIndex].date);
+                    return LineTooltipItem(
+                        "",
+                        children: [
+                          TextSpan(
+                              text:
+                                  "Price: ${prices[barSpot.spotIndex].price.toStringAsFixed(0)}\$ \n"),
+                          TextSpan(
+                              text: "Date: ${date.day}.${date.month}.${date.year}"),
+                        ],
+                        const TextStyle(color: Colors.white));
+                  },
+                ).toList();
+              },
+            ),
+          ),
+          lineBarsData: [
+            LineChartBarData(
+              color: Theme.of(context).primaryColor,
+              spots: [...getSpots()],
+              isCurved: false,
+              barWidth: 5,
+              isStrokeCapRound: true,
+              dotData: const FlDotData(
+                show: false,
+              ),
+              belowBarData: BarAreaData(
+                show: true,
+              ),
+            )
+          ],
         ),
-        minX: 0,
-        minY: 0,
-        titlesData: const FlTitlesData(
-          show: true,
-          rightTitles:  AxisTitles(
-            sideTitles: SideTitles(showTitles: false),
-          ),
-          topTitles:  AxisTitles(
-            sideTitles: SideTitles(showTitles: false),
-          ),
-          bottomTitles: AxisTitles(
-            sideTitles: SideTitles(
-              showTitles: false,
-              reservedSize: 30,
-              interval: 1,
-            ),
-          ),
-          leftTitles: AxisTitles(
-            sideTitles: SideTitles(
-              showTitles: false,
-              reservedSize: 42,
-            ),
-          ),
-        ),
-        lineBarsData: [
-          LineChartBarData(
-            spots: [...getSpots()],
-            isCurved: false,
-            barWidth: 5,
-            isStrokeCapRound: true,
-            dotData: const FlDotData(
-              show: false,
-            ),
-            belowBarData: BarAreaData(
-              show: true,
-            ),
-          )
-        ],
-      )),
+      ),
     );
   }
 
